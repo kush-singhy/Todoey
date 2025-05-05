bdf1c383ff203c568db96972dfdb2541578ce42b
Kush <153984588+kush-singhy@users.noreply.github.com>
2025-05-05 21:06:33 +1000

    Progress on project

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kush Singhy on 5/5/25.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
